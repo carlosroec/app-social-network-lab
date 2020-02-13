f7d33258eacfe8a9cce60865a0fbd79cd577c20c
Carlos G. Rodriguez <carlosro.ec@gmail.com>
2020-02-13 12:33:07 -0500

    Ignore console

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,8 @@ module.exports = {
       'max-len': ["error", { "code": 1000 }],
       'indent': ["error", 4],
       "no-param-reassign": [0],
-      "no-underscore-dangle": [0]
+      "no-underscore-dangle": [0],
+      "no-console":0,
     },
     overrides: [
       {
